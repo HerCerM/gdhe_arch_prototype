9a5ac01813e4447201d6ee8bc4282cf68e7810e7
Hernán Cervera <hjcerveram@hotmail.com>
2020-03-28 15:17:39 -0600

    Reduce DB schema and add populate.sql

---
+++ resources/populate.sql
@@ -0,0 +1,13 @@
+USE gdhe;
+
+INSERT INTO `admin` (`admin_id`, `name`, `first_last_name`, `second_last_name`, `password`)
+VALUES (NULL, 'Hernán', 'Cervera', 'Manzanilla', '123');
+
+INSERT INTO `admin` (`admin_id`, `name`, `first_last_name`, `second_last_name`, `password`)
+VALUES (NULL, 'Naomy', 'Padilla', 'García', '444');
+
+INSERT INTO `admin` (`admin_id`, `name`, `first_last_name`, `second_last_name`, `password`)
+VALUES (NULL, 'Elena', 'Gutiérrez', 'Carmona', '404');
+
+INSERT INTO `admin` (`admin_id`, `name`, `first_last_name`, `second_last_name`, `password`)
+VALUES (NULL, 'Dalia', 'Pacheco', 'Cuevas', '200');
